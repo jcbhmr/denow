d9371ce067d0e0d389b9cc1b09b9e40e95687abd
Jacob Hummer <jcbhmr@outlook.com>
2023-05-27 19:07:43 -0500

    Copyright 2023 Jacob Hummer

--- denow.bat
+++ denow.bat
@@ -1,5 +1,5 @@
-: https://github.com/chibat/deno-wrapper
-: Copyright (c) 2023 Tomofumi Chiba
+: https://github.com/jcbhmr/deno_wrapper
 : MIT License
+: Copyright (c) 2023 Jacob Hummer
 @echo off
 "%PROGRAMFILES%\Git\bin\sh.exe" denow %*
