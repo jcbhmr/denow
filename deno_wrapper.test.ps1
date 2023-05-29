dfdaf8d495a895727392306776e277e35766b8ca
Jacob Hummer <jcbhmr@outlook.com>
2023-05-29 15:19:48 -0500

    New test script

---
+++ deno_wrapper.test.ps1
@@ -0,0 +1,14 @@
+$owd = $PWD.Path
+$twd = New-TemporaryFile -Directory
+$null = Register-ObjectEvent -InputObject $twd -EventName Dispose -Action { Remove-Item -LiteralPath $twd.FullName -Recurse -Force } -SourceIdentifier "Cleanup"
+
+Set-Location -Path $twd.FullName
+
+Invoke-Expression "$(Get-Content "$owd/deno_wrapper.sh" -Raw)" -ArgumentList "1.30.0"
+if (-not (Test-Path "denow") -or -not (Test-Path "denow.bat")) { exit 1 }
+
+$v = (./denow eval 'console.log(Deno.version.deno)').Trim()
+if ($v -ne "1.30.0") { exit 1 }
+if (-not (Test-Path ".deno")) { exit 1 }
+
+Unregister-Event -SourceIdentifier "Cleanup"
