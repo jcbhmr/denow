d4058c1b83e955fa6cd916d3f3ca7f48a2665be9
Jacob Hummer <jcbhmr@outlook.com>
2023-05-29 15:25:58 -0500

    Try to make test script work?

--- deno_wrapper.test.ps1
+++ deno_wrapper.test.ps1
@@ -7,12 +7,12 @@ Function New-TemporaryFolder {
 
 $owd = $PWD.Path
 $twd = New-TemporaryFolder
-$null = Register-ObjectEvent -InputObject $twd -EventName Dispose -Action { Remove-Item -LiteralPath $twd.FullName -Recurse -Force } -SourceIdentifier "Cleanup"
 
 Set-Location -Path $twd.FullName
 
-Invoke-Expression "$(Get-Content "$owd/deno_wrapper.ps1" -Raw)" -ArgumentList "1.30.0"
-if (-not (Test-Path "denow") -or -not (Test-Path "denow.bat")) { exit 1 }
+Get-Content "$owd/deno_wrapper.ps1" -Raw | Invoke-Expression -ArgumentList "1.30.0"
+if (-not (Test-Path "denow")) { exit 1 }
+if (-not (Test-Path "denow.bat")) { exit 1 }
 
 $v = (./denow eval 'console.log(Deno.version.deno)').Trim()
 if ($v -ne "1.30.0") { exit 1 }
