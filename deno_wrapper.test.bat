de4e29eebedee1175516d20c403a0015b8b41915
Jacob Hummer <jcbhmr@outlook.com>
2023-05-28 21:49:51 -0500

    Apparently quoting matters?

--- deno_wrapper.test.bat
+++ deno_wrapper.test.bat
@@ -8,5 +8,5 @@ if not exist denow exit /b 1
 if not exist denow.bat exit /b 1
 
 for /f %%i in ('denow eval "console.log(Deno.version.deno)"') do set "v=%%i"
-if "%v%" neq 1.30.0 exit /b 1
+if "%v%" neq "1.30.0" exit /b 1
 if not exist .deno exit /b 1
