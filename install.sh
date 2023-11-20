c7fd44b777585c000cea00b1adb10c5e49d2236c
Jacob Hummer <jcbhmr@outlook.com>
2023-11-20 15:49:13 -0600

    do this

--- install.sh
+++ install.sh
@@ -14,25 +14,19 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
-if command -v curl 2> /dev/null; then
+if command -v curl &> /dev/null; then
   curl -fsSL "${base_url}denow.bat" -o ./denow.bat
   curl -fsSL "${base_url}denow" -o ./denow
-elif command -v wget 2> /dev/null; then
+else
   wget "${base_url}denow.bat" -O ./denow.bat
   wget "${base_url}denow" -O ./denow
-else
-  echo "Neither 'curl' nor 'wget' found." >&2
-  exit 1
 fi
 chmod +x ./denow
 
 if [ -n "$1" ]; then
   deno_version="$1"
-elif command -v grep 2> /dev/null; then
-  deno_version=$(get_latest_release denoland/deno | cut -c 2-)
 else
-  echo "Unable to fetch latest version" >&2
-  exit 1
+  deno_version=$(get_latest_release denoland/deno | cut -c 2-)
 fi
 
 sed -i "s/%__DENO_VERSION__%/$deno_version/g" ./denow.bat
