6ce3ec4b2bffec01bf31b6bebe3ed4a3fc75b315
Jacob Hummer <jcbhmr@outlook.com>
2023-11-20 16:20:24 -0600

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -2,10 +2,10 @@
 set -ex
 
 npm install --global serve
-serve &
+serve -p 7123 &
 sleep 3 # let server get ready
 
-export DENOW_DL_BASE_URL="http://localhost:7000/"
+export DENOW_DL_BASE_URL="http://localhost:7123/"
 
 if [[ $OS == Windows_NT ]]; then
   pwsh -Command "irm ${DENOW_DL_BASE_URL}install.ps1 | iex"
